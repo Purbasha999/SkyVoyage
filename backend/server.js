87366ccb77c5ec1a20c363ab97991a286735cc8f
Purbasha999 <purbasharoy2000@gmail.com>
2026-03-13 12:08:18 +0530

    Fix cors issue

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:3000', credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
